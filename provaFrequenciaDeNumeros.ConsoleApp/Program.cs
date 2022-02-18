29e8db5e25910b89d36a6592efc1d17e4dad52b1
lucasvs07 <lucasviniciusdasilva0@gmail.com>
2022-02-18 15:30:46 -0300

    Add project files.

---
+++ provaFrequenciaDeNumeros.ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace provaFrequenciaDeNumeros.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos números serão digitados?");
+            string strQuantidadeDeNumeros = Console.ReadLine();
+            int quantidadeDeNumeros = Convert.ToInt32(strQuantidadeDeNumeros);
+
+            int[] arrayNumeros = new int [quantidadeDeNumeros];
+            for (int i = 0; i < quantidadeDeNumeros; i++)
+            {
+                Console.WriteLine("Digite um número:");
+                string strNumero = Console.ReadLine();
+                int numero = Convert.ToInt32(strNumero);
+                arrayNumeros[i] = numero;
+
+            }
+            foreach (var item in arrayNumeros)
+            {
+                Console.Write(item.ToString());
+            }
+
+        }
+    }
+}
